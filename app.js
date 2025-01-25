4eb6d4dd3a16af0aee9ec8466006421b73cfa8c7
Castro456 <castrosid456@gmail.com>
2025-01-25 20:08:36 +0530

    Corrected variable names

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const env = require('dotenv');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,9 +19,11 @@ app.use(logger('dev')); // Give the incoming response logs
 env.config() // Without this line NODE_ENV values wont come from dotenv
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser()); //Get the cookie object for the request and can sign a cookie and send the response back
+//Get the cookie object for the request and can sign a cookie and send the response back
+app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -33,11 +34,13 @@ app.use(function(req, res, next) {
 
 // http-errors handler
 app.use(function(err, req, res, next) {
-  // set locals, uses local storage
+  // set locals, value exists only the duration of the current request
   // only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log(process.env.NODE_ENV); //by default the NODE_ENV=development. Here taking NODE_ENV from the dotenv module which refers .env file.
+
+  //By default the NODE_ENV=development (takes the value from the running process). Here taking NODE_ENV from the dotenv module which refers .env file.
+  console.log(process.env.NODE_ENV); 
 
   // render the error page
   res.status(err.status || 500);
